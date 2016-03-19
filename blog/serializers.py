799d02e481070e0d95934bb92063b169c772963a
Di Lu <ludiat16@gmail.com>
2016-03-19 11:29:15 -0700

    Add serializer to blogpost

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,14 +1,21 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from blog.models import BlogPost
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
+class BlogPostSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    user_id = serializers.IntegerField()
+    content = serializers.CharField()
+    title = serializers.CharField(max_length=100)
 
+    def create(self, validated_data):
+        return BlogPost.objects.create(**validated_data)
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
+    def update(self, instance, validated_data):
+
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.content = validated_data.get('content', instance.content)
+        instance.title = validated_data.get('title', instance.title)
+        instance.save()
+
+        return instance
