fb604cf9bf90900df011b0bb2d75bceec7099a43
Di Lu <ludiat16@gmail.com>
2016-03-20 13:11:31 -0700

    Add the first part of http response handling

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,20 +2,23 @@ from rest_framework import serializers
 from blog.models import BlogPost
 
 
-class BlogPostSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    user_id = serializers.IntegerField()
-    content = serializers.CharField()
-    title = serializers.CharField(max_length=100)
-
-    def create(self, validated_data):
-        return BlogPost.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-
-        instance.user_id = validated_data.get('user_id', instance.user_id)
-        instance.content = validated_data.get('content', instance.content)
-        instance.title = validated_data.get('title', instance.title)
-        instance.save()
-
-        return instance
+class BlogPostSerializer(serializers.ModelSerializer):
+    # pk = serializers.IntegerField(read_only=True)
+    # user_id = serializers.IntegerField()
+    # content = serializers.CharField(required=False)
+    # title = serializers.CharField(max_length=100)
+    #
+    # def create(self, validated_data):
+    #     return BlogPost.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #
+    #     instance.user_id = validated_data.get('user_id', instance.user_id)
+    #     instance.content = validated_data.get('content', instance.content)
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.save()
+    #
+    #     return instance
+    class Meta:
+        model = BlogPost
+        fields = ('created', 'user_id', 'content', 'title')
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,67 @@
-
 from django.http import HttpResponse
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from blog.serializers import UserSerializer, GroupSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from blog.models import BlogPost
+from blog.serializers import BlogPostSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+class JSONResponse(HttpResponse):
+
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def blog_post_list(request):
+    if request.method == 'GET':
+        blog_posts = BlogPost.objects.all()
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BlogPostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+
+        return JSONResponse(serializer.errors, status=400)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+@csrf_exempt
+def blog_post_detail(request, pk):
+    try:
+        blog_post = BlogPost.objects.get(pk=pk)
+    except BlogPost.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = BlogPostSerializer(blog_post)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BlogPostSerializer(sni)
 
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+#
+#
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows groups to be viewed or edited.
+#     """
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+#
+#
+#
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the blog index.")
